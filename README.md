c70c69382b2d4ff0904be9b646137bcabf769f6c
jamesncox <jamesncox@yahoo.com>
2019-10-31 09:55:15 -0500

    Add store div to index.html

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Run server on the frontend for Javascript
 python -m SimpleHTTPServer
+
+# Fetch stores
+http://localhost:3000/stores  
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-// fetch to the backend
+// fetch to the backend http://localhost:3000/stores
+
+function getStoreData() {
+    byebug
+    return fetch('http://localhost:3000/stores')
+        .then(res => res.json())
+        .then(data => {
+            allStores = data
+        })
+}
