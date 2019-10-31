93fc2868c54bdd095bf22220c0afa7e7105e8f73
jamesncox <jamesncox@yahoo.com>
2019-10-31 10:13:28 -0500

    Update store adapter class with constructor and fetch

--- src/adapters/StoresAdapter.js
+++ src/adapters/StoresAdapter.js
@@ -1,5 +1,11 @@
 class StoreAdapter {
     constructor() {
+        this.baseUrl =
+            'http://localhost:3000/store'
+    }
 
+    getStores() {
+        return fetch(this.baseUrl).then(res => res.json()
+        )
     }
 }
