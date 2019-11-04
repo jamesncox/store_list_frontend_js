fce706582a35371baccb8682aa73b2d41ddee535
jamesncox <jamesncox@yahoo.com>
2019-11-04 11:13:28 -0600

    Create render function in index.js

--- src/components/stores.js
+++ src/components/stores.js
@@ -7,8 +7,21 @@ class Stores {
     }
 
     fetchAndLoadStores() {
-        this.adapter.getStores().then(stores => {
-            console.log(stores)
-        })
+        this.adapter
+            .getStores()
+            .then(stores => {
+                // stores.forEach(store => this.stores.push(store))
+                return console.log(stores)
+            })
+            .then(() => {
+                this.render()
+            })
+    }
+
+    render() {
+        // console.log('rendering...')
+        const storesContainer = document.getElementById("stores-container")
+        storesContainer.innerHTML = 'my stores here'
+        // console.log('my stores are', this.stores)
     }
 }
