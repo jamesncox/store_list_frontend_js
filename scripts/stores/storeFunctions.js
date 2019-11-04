02ec5110542445003f49251653e280715868cdcb
jamesncox <jamesncox@yahoo.com>
2019-11-04 11:50:41 -0600

    Update storeFunction.js

--- scripts/stores/storeFunctions.js
+++ scripts/stores/storeFunctions.js
@@ -1,26 +1,15 @@
+const createStores = (list, stores) => {
+    stores.forEach((store) => {
+        let storeObj = new Store(store)
+        list.stores.push(storeObj)
+    })
+}
 
+// DJ's code....
 
-// const createObjs = (json) => {
-//     json.data.forEach((store) => {
-//         let store = new Store(store)
-//         let lists = json.included.filter((list) => {
-//             return list.relationships.store.data.id === store.id
-//         })
-//         createLists(store, lists)
-//         allStores.push(store)
+// const createSeasons = (teamMember, seasons) => {
+//     seasons.forEach((season) => {
+//         let seasonObj = new Season(season)
+//         teamMember.seasons.push(seasonObj)
 //     })
-//     // generate
-// }
-
-// const createObjs = (json) => {
-//     json.data.forEach((team_member) => {
-//         let teamMember = new TeamMember(team_member)
-//         let seasons = json.included.filter((season) => {
-//             return season.relationships.team_member.data.id === team_member.id
-//         })
-//         createSeasons(teamMember, seasons)
-//         // teamMember.seasons = seasons
-//         allMembers.push(teamMember)
-//     })
-//     generateTeamList(allMembers)
 // }
