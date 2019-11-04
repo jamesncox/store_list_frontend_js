1e5b3b5580d8f0bd0422bb8deb583bf79d732cbc
jamesncox <jamesncox@yahoo.com>
2019-11-04 11:40:04 -0600

    update Store class store.js

--- scripts/stores/store.js
+++ scripts/stores/store.js
@@ -1,7 +1,20 @@
-// class Store {
-//     construction(store) {
-//         this.id = store.id
-//         this.name = store.attributes.name
-//         this.lists = []
+class Store {
+    constructor(store) {
+        this.id = store.id
+        this.name = store.attributes.name
+        this.listId = store.relationships.list.data.id
+    }
+}
+
+// DJ's code....
+
+// class Season {
+//     constructor(season){
+//         this.id = season.id
+//         this.mmr = season.attributes.mmr
+//         this.kda = season.attributes.kda
+//         this.wl = season.attributes.wl 
+//         this.headShotRatio = season.attributes.headshot_ratio
+//         this.teammemberId = season.relationships.team_member.data.id
 //     }
 // }
