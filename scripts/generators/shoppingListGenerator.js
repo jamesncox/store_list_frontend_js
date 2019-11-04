442a255a69dc00709a20b5579ddd404d92dc7021
jamesncox <jamesncox@yahoo.com>
2019-11-04 14:50:02 -0600

    Add shoppingListGenerator.js

---
+++ scripts/generators/shoppingListGenerator.js
@@ -0,0 +1,41 @@
+DJ's code....
+const generateTeamList = (allMembers) => {
+    const containerEl = document.createElement("div")
+    const listEl = document.createElement("ul")
+
+
+    containerEl.classList.add("all-members")
+    listEl.classList.add("list")
+
+    if (allMembers.length > 0) {
+        allMembers.forEach((member => {
+            const itemEl = document.createElement("li")
+            itemEl.classList.add(`team-member-${member.uplay}`)
+            // renderTeam(member)
+            itemEl.textContent = member.uplay
+            listEl.appendChild(itemEl)
+        }))
+
+    } else {
+        containerEl.textContent = "Oops try again later!"
+    }
+
+    containerEl.appendChild(listEl)
+    main.append(containerEl)
+
+}
+
+// const generateShoppingList = (allLists) => {
+//     const containerEl = document.createElement("div")
+//     const listEl = document.createElement("ul")
+
+//     containerEl.classList.add("all-Lists")
+//     listEl.classList.add("list")
+
+//     if (allLists.length > 0){
+//         allLists.forEach((list => {
+//             const itemEl = document.createElement("li")
+//             itemEl.classList.add()
+//         }))
+//     }
+// }
