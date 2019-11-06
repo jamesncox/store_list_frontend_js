e2f62676158fe240ec25b81be6aa6dd2d90cdce9
jamesncox <jamesncox@yahoo.com>
2019-11-06 08:41:36 -0600

    Comment out DJ's code

--- scripts/generators/shoppingListGenerator.js
+++ scripts/generators/shoppingListGenerator.js
@@ -1,30 +1,30 @@
-DJ's code....
+// DJ's code....
 
-const generateTeamList = (allMembers) => {
-    const containerEl = document.createElement("div")
-    const listEl = document.createElement("ul")
+// const generateTeamList = (allMembers) => {
+//     const containerEl = document.createElement("div")
+//     const listEl = document.createElement("ul")
 
 
-    containerEl.classList.add("all-members")
-    listEl.classList.add("list")
+//     containerEl.classList.add("all-members")
+//     listEl.classList.add("list")
 
-    if (allMembers.length > 0) {
-        allMembers.forEach((member => {
-            const itemEl = document.createElement("li")
-            itemEl.classList.add(`team-member-${member.uplay}`)
-            // renderTeam(member)
-            itemEl.textContent = member.uplay
-            listEl.appendChild(itemEl)
-        }))
+//     if (allMembers.length > 0) {
+//         allMembers.forEach((member => {
+//             const itemEl = document.createElement("li")
+//             itemEl.classList.add(`team-member-${member.uplay}`)
+//             // renderTeam(member)
+//             itemEl.textContent = member.uplay
+//             listEl.appendChild(itemEl)
+//         }))
 
-    } else {
-        containerEl.textContent = "Oops try again later!"
-    }
+//     } else {
+//         containerEl.textContent = "Oops try again later!"
+//     }
 
-    containerEl.appendChild(listEl)
-    main.append(containerEl)
+//     containerEl.appendChild(listEl)
+//     main.append(containerEl)
 
-}
+// }
 
 // const generateShoppingList = (allLists) => {
 //     const containerEl = document.createElement("div")
