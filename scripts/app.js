2c7649f4a0c08345fdc363ffbb024bb8d930c83d
jamesncox <jamesncox@yahoo.com>
2019-11-11 13:08:46 -0600

    Change /store to /stores in all namespacing

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,7 @@
 let allStores = []
 
-const storeIndexUrl = "http://localhost:3000/store"
+const storeIndexUrl = "http://localhost:3000/stores"
 
 let main = document.querySelector("#stores-wrapper")
 
 fetchAllLists(storeIndexUrl)
-
-// DJ's code...
-
-// let allMembers = []
-
-// const teamIndexUrl = "http://localhost:3000/team_members"
-
-// let main = document.querySelector("#main")
--- scripts/eventListeners.js
+++ scripts/eventListeners.js
@@ -1,24 +1,30 @@
-// function submitNewStore() {
-//     const submitStore = document.getElementById('new-store-button');
-//     submitStore.addEventListener('click', submitNewStore(event))
-//     console.log("Click click click")
-// }
-
-// const newStoreSubmit = document.getElementById('new-store-submit');
-
-// function storeClick(e) {
-//     e.preventDefault()
-// }
-
-// newStoreSubmit.addEventListener('click', storeClick())
-
-// newStoreSubmit.addEventListener('click', function (e) {
-//     e.preventDefault()
-// })
-
 function getStoreName() {
     let storeName = document.getElementById('store-name').value
     document.getElementById('stores-wrapper').innherHTML = storeName
 }
 
 document.getElementById('submit-btn').addEventListener('click', getStoreName(console.log("click click click")))
+
+fetch("http://localhost:3000/stores")
+
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+
+//   let configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+
+//   fetch("http://localhost:3000/dogs", configObj)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(object) {
+//       console.log(object);
+//     });
