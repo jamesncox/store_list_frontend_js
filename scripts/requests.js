848d30b5dd8cfc9d52657f2fadb8ae956eb90803
jamesncox <jamesncox@yahoo.com>
2019-11-11 15:50:04 -0600

    Remove old cold

--- scripts/requests.js
+++ scripts/requests.js
@@ -3,11 +3,3 @@ const fetchAllLists = (storeIndexUrl) => {
         .then(resp => resp.json())
         .then(json => createObjs(json))
 }
-
-// DJ's code....
-
-// const fetchAllMembers = (teamIndexUrl) => {
-//     fetch(teamIndexUrl)
-//         .then(resp => resp.json())
-//         .then(json => createObjs(json))
-// }
